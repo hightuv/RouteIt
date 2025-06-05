4b9604601b476d73a263c43f5b38afcc2034053b
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-06-06 08:47:34 +0900

    feat: Place 엔티티 정의

--- src/place/entities/place.entity.ts
+++ src/place/entities/place.entity.ts
@@ -1,1 +1,40 @@
-export class Place {}
+import { Column, PrimaryColumn, Entity } from 'typeorm';
+
+@Entity('places')
+export class Place {
+  // 장소 고유 ID
+  @PrimaryColumn()
+  id: string;
+
+  // 장소 이름
+  @Column()
+  displayName: string;
+
+  // 주소
+  @Column()
+  formattedAddress: string;
+
+  // 장소 타입
+  @Column()
+  primaryTypeDisplayName: string;
+
+  // 사진 URL 배열
+  @Column('simple-array', { nullable: true })
+  photos: string[];
+
+  // 영업시간 요일별 배열
+  @Column('simple-array', { nullable: true })
+  weekdayDescriptions: string[];
+
+  // 사용자 평점
+  @Column()
+  userRatingCount: number;
+
+  // 리뷰
+  @Column('text', { nullable: true })
+  review: string;
+
+  // 국가 전화번호
+  @Column()
+  nationalPhoneNumber: string;
+}
