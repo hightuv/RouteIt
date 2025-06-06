1a28b81ec469f2b4f5815bd277359adb30c8677d
Minseo Park <hightuv@gmail.com>
2025-06-07 00:57:07 +0900

    :sparkles: feat: Implement basic Tag entity, and DTO structure
    - Added Tag entity with name field and ManyToMany relation to Route
    - Created TagResponseDto with validation

--- src/tag/entities/tag.entity.ts
+++ src/tag/entities/tag.entity.ts
@@ -1,1 +1,14 @@
-export class Tag {}
+import { Route } from 'src/route/entities/route.entity';
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Tag {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @ManyToMany(() => Route, (route) => route.tags)
+  routes: Route[];
+}
