99d0b87a5bdf0a85173b004f996c277b4502965f
Minseo Park <hightuv@gmail.com>
2025-06-07 01:07:51 +0900

    :sparkles: feat: Implement Route entity, module, and DTOs for CRUD operations
    - Added Route entity with fields and relations to Member, Tag, RoutePlace
    - Updated RouteModule to import PlaceModule for dependency injection
    - Created CreateRouteDto, DeleteRouteDto, and RouteResponseDto with validation

--- src/route/dto/create-route.dto.ts
+++ src/route/dto/create-route.dto.ts
@@ -1,1 +1,20 @@
-export class CreateRouteDto {}
+import { IsArray, IsBoolean, IsNumber, IsString } from 'class-validator';
+
+export class CreateRouteDto {
+  @IsString()
+  name: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  placeIds: string[];
+
+  @IsArray()
+  @IsNumber({}, { each: true })
+  tagIds: number[]; // Tag ID number 리스트가 넘어오게?
+
+  @IsBoolean()
+  isPublic: boolean;
+
+  @IsNumber()
+  memberId: number;
+}
--- src/route/route.module.ts
+++ src/route/route.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { RouteService } from './route.service';
 import { RouteController } from './route.controller';
+import { Route } from './entities/route.entity';
+import { RoutePlace } from './entities/route-place.entity';
+import { Tag } from 'src/tag/entities/tag.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Place } from 'src/place/entities/place.entity';
+import { Member } from 'src/member/entities/member.entity';
+import { PlaceModule } from 'src/place/place.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Route, RoutePlace, Place, Tag, Member]),
+    PlaceModule,
+  ],
   controllers: [RouteController],
   providers: [RouteService],
 })
