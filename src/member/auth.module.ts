7c700e3ab1cfb8b7dbafaf6d6bcaa890276046c3
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-06-08 04:14:47 +0900

    🛠️ chore: set up AuthModule with JWT and circular dependency handling

---
+++ src/member/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { MemberModule } from './member.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+    forwardRef(() => MemberModule),
+  ],
+  providers: [AuthService],
+  exports: [AuthService, JwtModule],
+})
+export class AuthModule {}
