f6e00c0437678e7c0e27d9d305978808e175729f
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-06-07 21:40:34 +0900

    ✨ feat: implement create and findOneByEmail method

--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,26 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Member } from './entities/member.entity';
 import { CreateMemberDto } from './dto/create-member.dto';
-import { UpdateMemberDto } from './dto/update-member.dto';
 
 @Injectable()
 export class MemberService {
-  create(createMemberDto: CreateMemberDto) {
-    return 'This action adds a new member';
+  constructor(
+    @InjectRepository(Member)
+    private readonly memberRepository: Repository<Member>,
+  ) {}
+
+  async create(createMemberDto: CreateMemberDto): Promise<Member> {
+    const member = this.memberRepository.create(createMemberDto);
+    return this.memberRepository.save(member);
   }
 
-  findAll() {
-    return `This action returns all member`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} member`;
-  }
-
-  update(id: number, updateMemberDto: UpdateMemberDto) {
-    return `This action updates a #${id} member`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} member`;
+  async findOneByEmail(email: string): Promise<Member | null> {
+    return this.memberRepository.findOne({ where: { email } });
   }
 }
