b7c8f9c3be8bb66d289eedd3ec327d12761ae07b
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-06-07 21:46:00 +0900

    ✨ feat: add AuthService and AuthController

--- src/member/member.module.ts
+++ src/member/member.module.ts
@@ -3,10 +3,12 @@ import { MemberService } from './member.service';
 import { MemberController } from './member.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from './entities/member.entity';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Member])],
-  controllers: [MemberController],
-  providers: [MemberService],
+  controllers: [MemberController, AuthController],
+  providers: [MemberService, AuthService],
 })
 export class MemberModule {}
