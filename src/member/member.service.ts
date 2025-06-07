53bf99644da19e7fedb6ce4a1e2cfb21b13f5dc1
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-06-08 04:10:43 +0900

    ✨ feat: add methods for email check and login

--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -11,12 +11,35 @@ export class MemberService {
     private readonly memberRepository: Repository<Member>,
   ) {}
 
+  // 사용자 생성
   async create(createMemberDto: CreateMemberDto): Promise<Member> {
     const member = this.memberRepository.create(createMemberDto);
     return this.memberRepository.save(member);
   }
 
+  // 이메일 존재 여부 확인
+  async existByEmail(email: string): Promise<boolean> {
+    const count = await this.memberRepository.count({ where: { email } });
+    return count > 0;
+  }
+
+  // 이메일 사용자 조회 (로그인 시 password가 필요한 경우)
+  async findOneByEmailWithPassword(
+    email: string,
+  ): Promise<Pick<Member, 'id' | 'email' | 'password'> | null> {
+    return this.memberRepository
+      .createQueryBuilder('member')
+      .addSelect('member.password')
+      .where('member.email = :email', { email })
+      .getOne();
+  }
+
   async findOneByEmail(email: string): Promise<Member | null> {
-    return this.memberRepository.findOne({ where: { email } });
+    return this.memberRepository
+      .createQueryBuilder('member')
+      .leftJoinAndSelect('member.routes', 'route')
+      .select(['member.email', 'route'])
+      .where('member.email = :email', { email })
+      .getOne();
   }
 }
