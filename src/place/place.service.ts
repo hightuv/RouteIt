7cd9b6a06e10bbec6656d2d8bcd8752e7d92a093
Minseo Park <hightuv@gmail.com>
2025-06-07 17:42:40 +0900

    :sparkles: feat: Add in-memory caching for place details using CacheManager
    - Checked cache before querying the database to reduce redundant DB access
    
    - Stored place details in cache after fetching from database or Google API

--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -1,5 +1,5 @@
 import { EntityManager, Repository } from 'typeorm';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Place } from './entities/place.entity';
 import { PlaceResponseDto } from './dto/place-response.dto';
@@ -8,10 +8,15 @@ import {
   searchPlacesFromGoogle,
 } from '../util/googlePlacesApi.util';
 import { mapGoogleDtoToPlaceEntity } from 'src/util/placeMapper.util';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class PlaceService {
   constructor(
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
+
     @InjectRepository(Place)
     private readonly placeRepository: Repository<Place>,
   ) {}
@@ -23,21 +28,35 @@ export class PlaceService {
 
   // id 기반 장소 상세 조회 (DB 저장 포함)
   async getPlaceDetails(id: string, manager?: EntityManager): Promise<Place> {
+    // 캐시 조회
+    let place = await this.cacheManager.get<Place>(`place:${id}`);
+    if (place) {
+      console.log(`캐시에서 장소 조회: ${id}`);
+      return place;
+    }
+
+    // DB 조회
     const placeRepository =
       manager?.getRepository(Place) || this.placeRepository;
 
-    let place = await placeRepository.findOne({ where: { id } });
+    place = await placeRepository.findOne({ where: { id } });
     if (place) {
       console.log('DB에서 장소 조회');
       return place;
     }
 
+    // Google Place API 조회
     const data: PlaceResponseDto = await fetchPlaceDetailsFromGoogle(id);
 
+    // DTO 전환
     place = mapGoogleDtoToPlaceEntity(data);
 
+    // DB 저장
     await placeRepository.save(place);
     console.log('DB에 장소 저장');
+
+    await this.cacheManager.set(`place:${id}`, place);
+    console.log('캐시에 장소 저장');
     return place;
   }
 }
