612f30d166aa787f003bd545d0f915f961585cad
Minseo Park <hightuv@gmail.com>
2025-06-10 01:13:22 +0900

    :sparkles: feat: Rename Member entity to User and add new columns and hashPassword functionality
    - Rename entity from Member to User throughout the codebase
    - Add refreshToken column to User entity
    - Implement hashPassword method with @BeforeInsert and @BeforeUpdate decorators for secure password storage

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,5 +1,13 @@
 import { Route } from 'src/route/entities/route.entity';
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Entity()
 export class User {
@@ -9,15 +17,24 @@ export class User {
   @Column({ unique: true })
   username: string;
 
+  @Column({ select: false })
+  password: string; // hashedPassword will be stored
+
+  @Column({ type: 'text', nullable: true })
+  refreshToken: string | null; // hashedRefreshToken will be stored
+
   @Column({ unique: true })
   email: string;
 
   @Column()
   name: string;
 
-  @Column({ select: false })
-  password: string;
-
   @OneToMany(() => Route, (route) => route.user)
   routes: Route[];
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
 }
