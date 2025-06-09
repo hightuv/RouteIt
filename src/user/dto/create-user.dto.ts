e48694e8351fafaebe6b0263661f3d1c3810c6c5
Minseo Park <hightuv@gmail.com>
2025-06-10 01:43:43 +0900

    :sparkles: feat: Add username field to CreateUserDto
    - Introduce username property with @IsNotEmpty and @IsString validation decorators
    - Update CreateUserDto to require username in addition to email, password, and name

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,10 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
   @IsNotEmpty({ message: '이메일 주소를 입력하세요.' })
   @IsEmail({}, { message: '유효한 이메일 형식이 아닙니다.' })
   email: string;
