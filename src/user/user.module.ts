affdee3e704e2af901e361b36ac188ee1f6a0e9c
Minseo Park <hightuv@gmail.com>
2025-06-10 20:05:02 +0900

    :sparkles: feat: Register related entities in UserModule for advanced relation queries
    - Add Route, RoutePlace, Place, and Tag entities to TypeOrmModule.forFeature in UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,13 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { Route } from 'src/route/entities/route.entity';
+import { RoutePlace } from 'src/route/entities/route-place.entity';
+import { Tag } from 'src/tag/entities/tag.entity';
+import { Place } from 'src/place/entities/place.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Route, RoutePlace, Place, Tag])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
